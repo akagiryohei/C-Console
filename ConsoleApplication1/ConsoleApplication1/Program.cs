df40e8db4eb405c200b26def6bbd2071521af3cd
ryohei_akagi <abbbbbbbb35@gmail.com>
2025-08-29 17:32:27 +0900

    デリゲート処理

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,40 +1,43 @@
 ﻿namespace ConsoleApplication1
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
 
     class Program
     {
+        /// <summary>
+        /// 入力引数なし、戻り値なしのデリゲート
+        /// </summary>
+        delegate void SomeDelegate1();
+
+        /// <summary>
+        /// int 型の入力引数、int型の戻り値のデリゲート
+        /// </summary>
+        /// <param name="i"></param>
+        delegate int SomeDelegate2(int i);
+
         static void Main(string[] args)
         {
-            var values = SampleObjects();
-            foreach (var value in values.Select((x, i) => new { x, i }))
-            {
-                Console.WriteLine("{0} 番目は {1} です。", value.i, value.x);
-            }
+            SomeDelegate1 d1 = Func1;
+            d1();
+
+            SomeDelegate2 d2 = Func2;
+            var d2_return = d2(3);
+            Console.WriteLine(d2_return + "が返ってきました。");
 
             Console.WriteLine("何かキーを押すと終了します。");
             Console.ReadKey();
         }
 
-        /// <summary>
-        /// あるコレクションを返すメソッドです。
-        /// </summary>
-        /// <returns>サンプル用のコレクションを返します。</returns>
-        private static IEnumerable<int> SampleObjects()
+        static void Func1()
         {
-            yield return 1;
-            yield return 2;
-            yield return 3;
-            yield return 4;
-            yield return 5;
-            yield return 6;
-            yield return 7;
-            yield return 8;
-            yield return 9;
-            yield return 10;
+            Console.WriteLine("Func1()が呼ばれました");
         }
-        
+
+        static int Func2(int x)
+        {
+            Console.WriteLine("Func2({0})が呼ばれました。", x);
+
+            return 2 * x;
+        }
     }
 }
