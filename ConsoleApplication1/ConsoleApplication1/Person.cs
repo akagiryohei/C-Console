9b13b32f8bcdcb85734f592fcd6f463a70492e93
ryohei_akagi <abbbbbbbb35@gmail.com>
2025-08-29 14:28:14 +0900

    Person.cs 最も略した形でのメソッド（get; set; )

--- ConsoleApplication1/ConsoleApplication1/Person.cs
+++ ConsoleApplication1/ConsoleApplication1/Person.cs
@@ -3,18 +3,9 @@
     internal class Person
     {
         /// <summary>
-        /// 発言内容
-        /// </summary>
-        private string _statement;
-
-        /// <summary>
         /// 発言内容を取得します。
         /// </summary>
         /// <returns>現在の発言を返します。</returns>
-        public string Statement
-        {
-            get { return this._statement; }
-            set { this._statement = value; }
-        }
+        public string Statement { get; set; }
     }
 }
