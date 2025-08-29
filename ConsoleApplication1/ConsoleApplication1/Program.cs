5a52f35b3962c843a7d627b54f47491ef92b74b4
ryohei_akagi <abbbbbbbb35@gmail.com>
2025-08-29 16:37:19 +0900

    for文とlengthで表現

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,22 +1,17 @@
 ﻿namespace ConsoleApplication1
 {
     using System;
+    using System.Collections.Generic;
 
     class Program
     {
         static void Main(string[] args)
         {
-            var statement = "おなかすいた。";
-
-            // Personクラスをインスタンス化
-            var person = new Person();
-            person.SpeakingToMyself(statement);
-            Console.WriteLine(person.Statement);
-
-            // PersonXクラスをインスタンス化
-            var personX = new PersonX();
-            personX.SpeakingToMyself(statement);
-            Console.WriteLine(personX.Statement);
+            var values = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            for (var i = 0; i < values.Length; i++)
+            {
+                Console.WriteLine(values[i]);
+            }
 
             Console.WriteLine("何かキーを押すと終了します。");
             Console.ReadKey();
