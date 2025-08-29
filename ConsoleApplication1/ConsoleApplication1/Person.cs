ddb82d5f67df1a565b34c71138f87be651c4f47c
ryohei_akagi <abbbbbbbb35@gmail.com>
2025-08-29 14:41:26 +0900

    Person.cs コンストラクタ作り

--- ConsoleApplication1/ConsoleApplication1/Person.cs
+++ ConsoleApplication1/ConsoleApplication1/Person.cs
@@ -3,6 +3,14 @@
     internal class Person
     {
         /// <summary>
+        /// 新しいインスタンスを生成します。
+        /// </summary>
+        public Person()
+        {
+            this.Statement = "どうも。";
+        }
+
+        /// <summary>
         /// 発言内容を取得します。
         /// </summary>
         /// <returns>現在の発言を返します。</returns>
