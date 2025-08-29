6c1f3740e9ea626563df439fba205523af22aadc
ryohei_akagi <abbbbbbbb35@gmail.com>
2025-08-29 17:06:54 +0900

    foreachとコレクション型の配列のときに何番目かを取得する方法

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -2,19 +2,39 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
 
     class Program
     {
         static void Main(string[] args)
         {
-            var values = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-            for (var i = 0; i < values.Length; i++)
+            var values = SampleObjects();
+            foreach (var value in values.Select((x, i) => new { x, i }))
             {
-                Console.WriteLine(values[i]);
+                Console.WriteLine("{0} 番目は {1} です。", value.i, value.x);
             }
 
             Console.WriteLine("何かキーを押すと終了します。");
             Console.ReadKey();
         }
+
+        /// <summary>
+        /// あるコレクションを返すメソッドです。
+        /// </summary>
+        /// <returns>サンプル用のコレクションを返します。</returns>
+        private static IEnumerable<int> SampleObjects()
+        {
+            yield return 1;
+            yield return 2;
+            yield return 3;
+            yield return 4;
+            yield return 5;
+            yield return 6;
+            yield return 7;
+            yield return 8;
+            yield return 9;
+            yield return 10;
+        }
+        
     }
 }
